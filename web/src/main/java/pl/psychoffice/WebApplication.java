1ea1c4b211091174b82895f5f52b573a0fbbf85b
Mariusz Pawlowski <mario@data.pl>
2015-08-05 00:29:47 +0200

    empty web app with security and simple template

---
+++ web/src/main/java/pl/psychoffice/WebApplication.java
@@ -0,0 +1,20 @@
+package pl.psychoffice;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+//@RestController
+@SpringBootApplication
+public class WebApplication {
+
+   // @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
