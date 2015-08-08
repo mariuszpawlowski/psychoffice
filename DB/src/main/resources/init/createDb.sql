de29d43e727c9a6b18da498f2b42183549ac0cd8
Mariusz Pawlowski <mario@data.pl>
2015-08-08 22:05:29 +0200

    DB initial commit

---
+++ DB/src/main/resources/init/createDb.sql
@@ -0,0 +1,9 @@
+DROP DATABASE psychoffice;
+
+CREATE DATABASE psychoffice
+  WITH OWNER = psychoffice
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'pl_PL.UTF-8'
+       LC_CTYPE = 'pl_PL.UTF-8'
+       CONNECTION LIMIT = -1;
