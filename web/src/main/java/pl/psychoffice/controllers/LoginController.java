6aed5c228839a2555f954d59ab2d957f4365d553
Mariusz Pawlowski <mario@data.pl>
2015-08-21 07:47:30 +0200

    initial login version

---
+++ web/src/main/java/pl/psychoffice/controllers/LoginController.java
@@ -0,0 +1,16 @@
+package pl.psychoffice.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by mario on 20/08/15.
+ */
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    String admin() {
+        return "login";
+    }
+}
