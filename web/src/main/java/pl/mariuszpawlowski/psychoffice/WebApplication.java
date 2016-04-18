1514d3ff2ab48317c4043dc98f940efc0fddcb9f
Mariusz Pawlowski <mario@data.pl>
2016-04-18 14:30:32 +0200

    code clean up

--- web/src/main/java/pl/mariuszpawlowski/psychoffice/WebApplication.java
+++ web/src/main/java/pl/mariuszpawlowski/psychoffice/WebApplication.java
@@ -1,6 +1,8 @@
 package pl.mariuszpawlowski.psychoffice;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
--- web/src/main/java/pl/mariuszpawlowski/psychoffice/security/Roles.java
+++ web/src/main/java/pl/mariuszpawlowski/psychoffice/security/Roles.java
@@ -4,11 +4,6 @@ package pl.mariuszpawlowski.psychoffice.security;
  * Created by mario on 21/08/15.
  */
 public class Roles {
-
-    public static String USER = "user";
-    public static String ADMIN = "admin";
-
-    public static String GRANTED_USER = "ROLE_user";
-    public static String GRANTED_ADMIN = "ROLE_admin";
-
+    public static String USER = "USER";
+    public static String ADMIN = "ADMIN";
 }
