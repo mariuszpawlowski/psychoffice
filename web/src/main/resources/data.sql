01ca35d08395062f748f60503a8f866109147b7e
Mariusz Pawlowski <mario@data.pl>
2016-04-20 14:45:05 +0200

    adding postgres - not fully working

--- web/src/main/resources/data.sql
+++ web/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO user (email, password_hash, role) VALUES ('admin@localhost', '$2a$10$WUmZqgjhdcXKlo9YXif8Iu8NmpLznTceZehg6DPYkB4xc5cFNVGnO', 'ADMIN');
+--INSERT INTO user (email, password_hash, role) VALUES ('admin@localhost', '$2a$10$WUmZqgjhdcXKlo9YXif8Iu8NmpLznTceZehg6DPYkB4xc5cFNVGnO', 'ADMIN');
 
 INSERT INTO user_details (name, surname, phone, city) VALUES ('name', 'surname', 'phone', 'city');
 -- INSERT INTO user (email, password_hash, role, user_detail_id) VALUES ('user@localhost', '$2a$10$WUmZqgjhdcXKlo9YXif8Iu8NmpLznTceZehg6DPYkB4xc5cFNVGnO', 'USER',
