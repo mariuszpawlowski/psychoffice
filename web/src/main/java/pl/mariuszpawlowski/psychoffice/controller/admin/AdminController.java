13571dd201f9fcf728699fa681bce99a7683ce6e
Mariusz Pawlowski <mario@data.pl>
2016-04-27 16:17:33 +0200

    semi working two sites in admin

--- web/src/main/java/pl/mariuszpawlowski/psychoffice/controller/admin/AdminController.java
+++ web/src/main/java/pl/mariuszpawlowski/psychoffice/controller/admin/AdminController.java
@@ -2,7 +2,9 @@ package pl.mariuszpawlowski.psychoffice.controller.admin;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Mariusz.Pawlowski on 2015-08-11.
@@ -12,7 +14,21 @@ public class AdminController {
 
     @PreAuthorize("hasAuthority('ADMIN')")
     @RequestMapping("/admin")
-    String admin() {
+    public String admin() {
         return "admin/admin";
     }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @RequestMapping("/admin/showClients")
+    public String showClients(Model model) {
+        return "admin/showClients";
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @RequestMapping("/admin/showVisits")
+    public ModelAndView showVisits() {
+        return new ModelAndView("admin/showVisits");
+    }
+
+
 }
