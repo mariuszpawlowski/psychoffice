77147fd503a6f459aae9a01e6be655a28ddbe19f
Mariusz Pawlowski <pawlowski.mariusz@gmail.com>
2018-03-30 16:19:17 +0200

    Turn off DB i encryption

--- web/src/main/java/pl/mariuszpawlowski/psychoffice/WebApplication.java
+++ web/src/main/java/pl/mariuszpawlowski/psychoffice/WebApplication.java
@@ -1,14 +1,12 @@
 package pl.mariuszpawlowski.psychoffice;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication
-@EnableEncryptableProperties
-@PropertySource(name = "EncryptedProperties", value = "classpath:encrypted.properties")
+//@EnableEncryptableProperties
+//@PropertySource(name = "EncryptedProperties", value = "classpath:encrypted.properties")
 public class WebApplication {
 
     public static void main(String[] args) throws Exception {
